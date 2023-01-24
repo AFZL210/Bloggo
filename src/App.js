acc97c1f0cfff0c8488617f57f74417769079c61
Afzal Khan <ak341668@gmail.com>
2023-01-24 15:43:08 +0530

    Created myBloggingApp CRA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+   <main>
+    <header>
+      <a href="#" className='logo'>Bloggo</a>
+      <nav>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+      </nav>
+    </header>
+
+    <div className="post">
+      <div className="image">
+        <img src="https://res.cloudinary.com/primeflix/image/upload/v1674554007/download_w0satf.jpg" alt="blog-post-image" />
+      </div>
+      <div className="texts">
+        <h2>SpaceX starship is ready to takeoff in 2023</h2>
+        <p className="info">
+          <a href="#" className="author">Afzal Khan</a>
+          <time>2023-03-24 16:34</time>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className='summary'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet laborum veniam nostrum ipsa exercitationem quos quisquam laudantium tenetur consectetur vero!</p>
+      </div>
     </div>
+
+    <div className="post">
+      <div className="image">
+        <img src="https://res.cloudinary.com/primeflix/image/upload/v1674554007/download_w0satf.jpg" alt="blog-post-image" />
+      </div>
+      <div className="texts">
+        <h2>SpaceX starship is ready to takeoff in 2023</h2>
+        <p className="info">
+          <a href="#" className="author">Afzal Khan</a>
+          <time>2023-03-24 16:34</time>
+        </p>
+        <p className='summary'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet laborum veniam nostrum ipsa exercitationem quos quisquam laudantium tenetur consectetur vero!</p>
+      </div>
+    </div>
+
+    <div className="post">
+      <div className="image">
+        <img src="https://res.cloudinary.com/primeflix/image/upload/v1674554007/download_w0satf.jpg" alt="blog-post-image" />
+      </div>
+      <div className="texts">
+        <h2>SpaceX starship is ready to takeoff in 2023</h2>
+        <p className="info">
+          <a href="#" className="author">Afzal Khan</a>
+          <time>2023-03-24 16:34</time>
+        </p>
+        <p className='summary'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet laborum veniam nostrum ipsa exercitationem quos quisquam laudantium tenetur consectetur vero!</p>
+      </div>
+    </div>
+
+  
+   </main>
   );
 }
 
