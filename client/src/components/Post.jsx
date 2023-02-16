8a3f4ae66435365a836779b39943acb173c6ad40
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-02-16 23:08:11 +0530

    added file upload

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import '../App.css'
+import { format } from 'date-fns'
 
-const Post = () => {
+const Post = ({ title, summary, content, cover, createdAt }) => {
   return (
     <div className="post">
     <div className="image">
       <img src="https://res.cloudinary.com/primeflix/image/upload/v1674554007/download_w0satf.jpg" alt="blog-post-image" />
     </div>
     <div className="texts">
-      <h2>SpaceX starship is ready to takeoff in 2023</h2>
+      <h2>{title}</h2>
       <p className="info">
         <a href="#" className="author">Afzal Khan</a>
-        <time>2023-03-24 16:34</time>
+        <time>{format(new Date(createdAt), 'MMM d yyyy HH:mm')}</time>
       </p>
-      <p className='summary'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet laborum veniam nostrum ipsa exercitationem quos quisquam laudantium tenetur consectetur vero!</p>
+      <p className='summary'>{summary}</p>
     </div>
   </div>
   )
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react'
 import './../App.css'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
 
-  const [title, setTitle] = useState('')
-  const [summary, setSummary] = useState('')
-  const [content, setContent] = useState('')
-  const [files, setFiles] = useState('')
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const navigate = useNavigate();
 
-  const createNewPost = (e) => {
+  const createNewPost = async(e) => {
     const postData = new FormData();
     postData.set('title', title);
     postData.set('summary', summary);
@@ -19,11 +21,18 @@ const CreatePost = () => {
 
     e.preventDefault();
     
-    fetch('http://localhost:5000/post/newpost', {
+    const response = await fetch('http://localhost:5000/post/newpost', {
       method: 'POST',
       body: postData,
+      credentials: 'include',
     })
+    
+    if(response.ok) {
+      navigate('/')
+    }
+
   }
+
 
   return (
     <div className="create-post-container">
