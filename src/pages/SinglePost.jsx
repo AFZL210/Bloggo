bcec47dba781e07e51a78e46e36cf47c64dd1331
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-02-18 17:39:21 +0530

    fixed singlePost

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -3,17 +3,20 @@ import React, { useEffect, useState, useContext } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { format } from 'date-fns'
 import { UserContext } from './../UserContext'
+import { FallingLines } from 'react-loader-spinner'
 
 const SinglePost = () => {
    const postId = useParams().id;
    const [postData, setPostData] = useState(null);
    const { userInfo } = useContext(UserContext);
+   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_HEAD}/post/${postId}`)
         .then(res => {
             res.json().then(postData => {
                 setPostData(postData);
+                setLoading(false)
             })
         })
   }, [])  
@@ -23,18 +26,27 @@ const SinglePost = () => {
 
   return (
     <div className='post-container'>
-        <h1>{postData.title}</h1>
-            <time>{format(new Date(postData.createdAt), 'MMM d yyyy HH:mm')}</time>
-            <p className='author'>by @{postData.author.username}</p>
-            {
-                userInfo.username === postData.author.username && (
-                    <div className='edit-row'><Link to={`/edit/${postData._id}`} className='edit-post'>Edit Post</Link></div>
-                )
-            }
-        <div className="image">
-            <img src={`${process.env.REACT_APP_SERVER_HEAD}/${postData.cover}`} alt="img" />
-        </div>
-        <div dangerouslySetInnerHTML={{__html:postData.content}}/>
+        {loading ? (<div style={{ width: '100%', height: '70vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><FallingLines
+        color="#551A8B"
+        width="60"
+        visible={true}
+        ariaLabel='falling-lines-loading'
+      /></div>) : (
+            <>
+            <h1>{postData.title}</h1>
+                <time>{format(new Date(postData.createdAt), 'MMM d yyyy HH:mm')}</time>
+                <p className='author'>by @{postData.author.username}</p>
+                {
+                    userInfo.username === postData.author.username && (
+                        <div className='edit-row'><Link to={`/edit/${postData._id}`} className='edit-post'>Edit Post</Link></div>
+                    )
+                }
+            <div className="image">
+                <img src={`${process.env.REACT_APP_SERVER_HEAD}/${postData.cover}`} alt="img" />
+            </div>
+            <div dangerouslySetInnerHTML={{__html:postData.content}}/>
+            </>
+        )}
     </div>
   )
 }
